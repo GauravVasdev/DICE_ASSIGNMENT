fef1b1bc8df1970a3c39e30871a76edc2378fe0c
GauravVasdev <gaurav.vasdev@hexaviewtech.com>
2024-04-11 19:10:17 +0530

    add constant before get

--- common-utility-service/src/main/java/com/dice/commonservice/constant/CommonServiceConstant.java
+++ common-utility-service/src/main/java/com/dice/commonservice/constant/CommonServiceConstant.java
@@ -3,9 +3,11 @@ package com.dice.commonservice.constant;
 public class CommonServiceConstant {
     public static final String BASE_URL = "/common";
 
-    public static final String GET_FORECAST_SUMMARY_BY_LOCATION = "/getforecast/summary/location/{country}";
-    public static final String GET_HOURLY_FORECAST_SUMMARY_BY_LOCATION = "/gethourlyforecast/summay/location/{country}";
-    public static final String GET_TWEETS_BY_USERNAME = "/tweets/getbyusername/{username}";
+    public static final String GET_BASE_URL = "/get/";
 
-    public static final String GET_SEARCH_TWEET_USER = "/tweets/getbyquery/{query}";
+    public static final String GET_FORECAST_SUMMARY_BY_LOCATION = GET_BASE_URL+"/forecast/summary/location/{country}";
+    public static final String GET_HOURLY_FORECAST_SUMMARY_BY_LOCATION = GET_BASE_URL + "/hourly-forecast/summary/location/{country}";
+    public static final String GET_TWEETS_BY_USERNAME = GET_BASE_URL + "/tweets/getbyusername/{username}";
+
+    public static final String GET_SEARCH_TWEET_USER = GET_BASE_URL + "/tweets/getbyquery/{query}";
 }
